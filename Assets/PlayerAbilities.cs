c6702892f7175287a4a7a35110ae1e9709fcb481
Alberto Alvarado Jr <aajrgs@gmail.com>
2023-05-26 00:46:36 -0500

    Finished changes

--- Assets/PlayerAbilities.cs
+++ Assets/PlayerAbilities.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+#region
+//Created by Alberto Alvarado Jr
+#endregion
 public class PlayerAbilities : MonoBehaviour
 {
+    [SerializeField] private Slider slide;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
     private Animator anim;
-    public bool jumping;
+    private bool jumping,readyToJump;
     private Rigidbody rb;
-    public float jumpAmount = 20;
-    private float jumpTime , buttonTime=0.3f;
-    public Slider slide;
+    private float jumpAmount = 2f;
+    private float jumpTime, kickTime, buttonTime = 0.3f;
     private void Start()
     {
         anim = GetComponent<Animator>();
@@ -19,41 +24,56 @@ public class PlayerAbilities : MonoBehaviour
 
     private void Update()
     {
+        //if jumping then adds force upwards and activates timers for kick and jump time
         if (jumping)
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpAmount, rb.velocity.z);
             jumpTime += Time.deltaTime;
+            kickTime += Time.deltaTime;
         }
+        //sets the amount of time you're allowed to continue ascending
         if (jumpTime > buttonTime)
-        {
             jumping = false;
-            anim.SetBool("KickTime", false);
 
-        }
-        if (slide.value == 1)
-            Kick(); 
+        //activates kick
+        if (slide.value == 1 && kickTime < buttonTime)
+            Kick();
+
     }
 
     public void Jump()
     {
-        anim.SetTrigger("Jump");
-        jumping = true;
-        jumpTime = 0;
+        if (IsGrounded() && readyToJump)
+        {
+            readyToJump = false;
+            anim.SetTrigger("Jump");
+            jumping = true;
+            jumpTime = 0;
+        }
     }
 
     public void Descending()
     {
+        //player falling
         jumping = false;
-        anim.SetBool("KickTime", false);
+        kickTime = 0;
+    }
+    public void ReadyToJump()
+    {
+        readyToJump = true;
     }
 
     public void Kick()
     {
-        //if(jumping &&  jumpTime < buttonTime)
-        //{
-        anim.SetBool("KickTime", true);
-            Debug.Log("Kick");
+        //certain amount of time the player can kick when in the air.
+        if(!IsGrounded())
             anim.SetTrigger("Kick");
-        //}
     }
-}
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
+    }
+ }
+    
+
